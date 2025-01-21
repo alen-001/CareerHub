866d83a40a63a50122c9b0ff408f88f14c3911d8
alen <132469401+alen-001@users.noreply.github.com>
2025-01-21 18:46:12 +0530

    Bento grid and Call to action

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,16 @@ import Nav from './Components/Nav';
 import VideoBackground from './Components/VideoBg';
 import Hero from './Pages/Hero';
 import Feature from './Pages/Feature';
-
+import Bento from './Pages/Bento';
+import CallToAction from './Pages/CallToAction';
 function App() {
   return (
-    <div className='w-screen'>
+    <div className='w-screen bg-black'>
         <div className='sticky top-0 z-50'><Nav></Nav></div>
         <div>
         <Hero/>
-        <Feature/>
+        <Bento/>
+        <CallToAction/>
         </div>
     </div>
   );
--- frontend/src/Components/Nav.jsx
+++ frontend/src/Components/Nav.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-
+import logo from '../assets/logo.png';
 function Nav() {
     const [scrollY, setScrollY] = useState(0);
     const [maxScrollY, setMaxScrollY] = useState(0);
@@ -33,15 +33,17 @@ function Nav() {
         <nav
             className="w-11/12 z-50 absolute top-0 left-1/2 transform -translate-x-1/2 my-10 flex justify-between items-center h-16  text-white shadow-sm font-mono p-10 rounded-2xl"
             role="navigation"
-            style={{ backgroundColor: `rgba(0,0,0,${Math.min(scrollY / 100, 1)})` }}
+            style={{ backgroundColor: `rgba(0,0,0,${Math.min(scrollY / (maxScrollY*0.6), 1)})` }}
         >
-            <div className="font-extralight text-xl">CareerHub</div>
+            <div className="flex items-center">
+            <img src={logo} className='h-16'></img>
+            <div className="font-extralight text-xl">CareerShepherd</div>
+            </div>
             <div className="m-0 p-0 flex space-x-10">
-                <div className="flex items-center">Resume Parser</div>
-                <div className="flex items-center">Personalized Roadmaps</div>
+
                 <div className="relative flex flex-col items-center">
-                    <div>Login</div>
-                    <span className="w-12 bg-gradient-to-r from-transparent via-red-400 to-transparent h-px mt-1" />
+                    <div>Login/Register</div>
+                    <span className="w-20 bg-gradient-to-r from-transparent via-red-400 to-transparent h-px mt-1" />
                 </div>
             </div>
             <span
